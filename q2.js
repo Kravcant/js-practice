ca86057f58dde947e2a1c83373fb34f4cc6908ed
anthonykravchishin <tonykravc53@gmail.com>
2025-10-21 22:26:46 -0700

    Added functionality to findLargest function; Added extra test arrays

--- q2.js
+++ q2.js
@@ -1,9 +1,21 @@
 // 2. Find the largest number in an array (using a loop)
 function findLargest(array) {
-
+    if (array.length < 1) {
+        return null;
+    }
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (i > max) {
+            max = array[i];
+        }
+    }
+    return max;
 }
 
 // Test for find largest
-let testArray2 = [1, 4, 5, 4, 3]; // 5
+let testArray1 = [1, 4, 5, 4, 3]; // 5
+let testArray2 = []; // null
 
 // TODO: display the largest number in the array in the console
+console.log(findLargest(testArray1));
+console.log(findLargest(testArray2));
